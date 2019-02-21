f883f0ad921673462f26f7e2c09e6d32fe68f133
Markus <marpe163@student.liu.se>
2019-02-21 22:33:12 +0100

    Initial commit for libcomponent. Essential functionality working.

---
+++ scr/libcomponent/libcomponent.c
@@ -0,0 +1,102 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+static const float e12_series_coefficients[] = {1.f, 1.2f, 1.5f, 1.8f, 2.2f, 2.7f, 3.3f, 3.9f, 4.7f, 5.6f, 6.8f, 8.2f};
+
+typedef struct e12_series_
+{
+    int number_of_resistors;
+    float * resistor_values;    
+} e12_series;
+
+static e12_series generate_e12_series(const float orig_resistance);
+static int get_number_of_resistors_in_series(const float orig_resistance);
+int e_resistance(float orig_resistance, float *res_array );
+
+int main()
+{
+    float res[3];
+    int count = e_resistance(1398.f, res);
+
+   return 0;
+}
+
+int e_resistance(float orig_resistance, float *res_array )
+{
+    e12_series e12Resistors = generate_e12_series(orig_resistance);
+    int number_of_resistors = 0;
+    float current_e_res = -1.f;
+    float res_difference;
+    float abs_error;
+    float best_diff = orig_resistance;
+    for(int i=0; i < e12Resistors.number_of_resistors; i++)
+    {
+        for(int j=i; j < e12Resistors.number_of_resistors; j++)
+        {
+            for(int k=j; k < e12Resistors.number_of_resistors; k++)
+            {
+                current_e_res = e12Resistors.resistor_values[i] + 
+                                e12Resistors.resistor_values[j] +
+                                e12Resistors.resistor_values[k];
+                res_difference = orig_resistance - current_e_res;
+                abs_error = res_difference < 0 ? res_difference * -1.f : res_difference;
+                
+                if(abs_error < best_diff)
+                {
+                    best_diff = abs_error;
+                    res_array[0]=e12Resistors.resistor_values[k];
+                    res_array[1]=e12Resistors.resistor_values[j];
+                    res_array[2]=e12Resistors.resistor_values[i];
+                    number_of_resistors = 0;
+
+                    for(int q=0; q<3;q++)
+                    {
+                        if(res_array[q] > 0)
+                        {
+                            number_of_resistors++;
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+   free(e12Resistors.resistor_values);
+   return number_of_resistors; 
+}
+
+static e12_series generate_e12_series(const float orig_resistance)
+{
+    int number_of_resistorsInSeries = get_number_of_resistors_in_series(orig_resistance);
+    e12_series resistors = {.number_of_resistors = number_of_resistorsInSeries,
+                            .resistor_values = calloc(number_of_resistorsInSeries, sizeof(float))};
+
+    for(int resistor_idx=0; resistor_idx<number_of_resistorsInSeries - 1; resistor_idx++)
+    {
+        resistors.resistor_values[resistor_idx + 1] = e12_series_coefficients[resistor_idx % 12] * powf(10.f, (resistor_idx / 12));
+    }
+
+    return resistors; 
+}
+
+static int get_number_of_resistors_in_series(const float orig_resistance)
+{
+    int number_of_resistors = 0;
+    float currentVal;
+
+    for(int resistor_idx = 0;;resistor_idx++)
+    {
+        currentVal = e12_series_coefficients[resistor_idx % 12] * powf(10.f, resistor_idx / 12);
+
+        number_of_resistors++;
+
+        if(currentVal > orig_resistance)
+        {
+             break;
+        }
+    }
+    number_of_resistors++; //Include the zero-resistor
+    return number_of_resistors;
+}
+
